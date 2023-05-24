e7080c1496ce7640e072be1cd09e2fe11bc663ba
dosymep <dosymep@gmail.com>
2023-05-24 16:40:06 +0600

    update classes property

--- Bim4EveryoneTelemetry/Models/Scripts/EngineInfo.cs
+++ Bim4EveryoneTelemetry/Models/Scripts/EngineInfo.cs
@@ -1,3 +1,3 @@
 namespace Bim4EveryoneTelemetry.Models.Scripts; 
 
-public record EngineInfo(string Type, Version Version, string[] SysPaths, Dictionary<string, object> Configs);
+public record EngineInfo(string Type, string Version, string[]? SysPaths, object? Configs);
