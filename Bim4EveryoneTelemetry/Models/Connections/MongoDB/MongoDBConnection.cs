332229366b97e0710c7ddf0ff4efbbe328fb30dd
dosymep <dosymep@gmail.com>
2023-05-25 17:00:09 +0600

    add get mongo version

--- Bim4EveryoneTelemetry/Models/Connections/MongoDB/MongoDBConnection.cs
+++ Bim4EveryoneTelemetry/Models/Connections/MongoDB/MongoDBConnection.cs
@@ -3,35 +3,40 @@ using Bim4EveryoneTelemetry.Models.Scripts;
 
 using Microsoft.Extensions.Options;
 
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Bim4EveryoneTelemetry.Models.Connections.MongoDB;
 
 public class MongoDBConnection : IDBConnectionStatus, IRepository<ScriptRecord>, IRepository<EventRecord> {
     private readonly ILogger<MongoDBConnection> _logger;
+    private readonly IMongoClient _mongoClient;
+    private readonly IMongoDatabase _mongoDatabase;
     private readonly IMongoCollection<EventRecord> _eventsCollection;
     private readonly IMongoCollection<ScriptRecord> _scriptCollection;
 
     public MongoDBConnection(ILogger<MongoDBConnection> logger, IOptions<MongoDBSettings> mongoDBSettings) {
         _logger = logger;
         
-        var mongoClient = new MongoClient(
+        _mongoClient = new MongoClient(
             mongoDBSettings.Value.ConnectionString + $"/?authSource={mongoDBSettings.Value.DatabaseName}");
 
-        var mongoDatabase = mongoClient.GetDatabase(
+        _mongoDatabase = _mongoClient.GetDatabase(
             mongoDBSettings.Value.DatabaseName);
 
-        _eventsCollection = mongoDatabase.GetCollection<EventRecord>(
+        _eventsCollection = _mongoDatabase.GetCollection<EventRecord>(
             mongoDBSettings.Value.EventsCollectionName);
         
-        _scriptCollection = mongoDatabase.GetCollection<ScriptRecord>(
+        _scriptCollection = _mongoDatabase.GetCollection<ScriptRecord>(
             mongoDBSettings.Value.ScriptsCollectionName);
     }
 
     public string ConnectionName => "mongodb";
 
     public ConnectionStatus GetConnectionStatus() {
-        return new ConnectionStatus(Status: "pass", Version: new Version("1.0.0"));
+        var command = new BsonDocumentCommand<BsonDocument>(new BsonDocument() {{"buildInfo", 1}});
+        var version = _mongoDatabase.RunCommand(command)["version"];
+        return new ConnectionStatus(Status: "pass", Version: new Version(version.AsString));
     }
 
     public async Task CreateAsync(ScriptRecord item) {
